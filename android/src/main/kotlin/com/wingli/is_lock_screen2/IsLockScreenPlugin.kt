18ba173e05671e6e97ecb6fb7e7bab2c37561a64
Nazildo Souza <nsouza10@outlook.com>
2025-02-18 00:25:27 -0300

    fix .kt

--- android/src/main/kotlin/com/wingli/is_lock_screen2/IsLockScreenPlugin.kt
+++ android/src/main/kotlin/com/wingli/is_lock_screen2/IsLockScreenPlugin.kt
@@ -4,51 +4,38 @@ import android.app.KeyguardManager
 import android.content.Context
 import android.os.Build
 import android.os.PowerManager
-import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** IsLockScreenPlugin */
-public class IsLockScreenPlugin(val registrarContext: Context? = null) : FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
-  private var bindingContext : Context? = null
+public class IsLockScreenPlugin : FlutterPlugin, MethodCallHandler {
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    bindingContext = flutterPluginBinding.applicationContext
-    channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "is_lock_screen")
-    channel.setMethodCallHandler(this)
+  private var bindingContext: Context? = null
+  private var methodChannel: MethodChannel? = null
+
+
+  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    this.bindingContext = binding.applicationContext
+    methodChannel = MethodChannel(binding.binaryMessenger, "is_lock_screen")
+    methodChannel!!.setMethodCallHandler(this)
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "is_lock_screen")
-      channel.setMethodCallHandler(IsLockScreenPlugin(registrarContext = registrar.activeContext()))
-    }
+  override fun onDetachedFromEngine(binding: FlutterPluginBinding) {
+    bindingContext = null
+    methodChannel!!.setMethodCallHandler(null)
+    methodChannel = null
+
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+  override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
     when (call.method) {
       "isLockScreen" -> {
-        val context = bindingContext ?: registrarContext
+        val context = bindingContext
         ?: return result.error("NullContext", "Cannot access system service as context is null", null)
 
         val keyguardManager: KeyguardManager = context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
@@ -72,7 +59,4 @@ public class IsLockScreenPlugin(val registrarContext: Context? = null) : Flutter
     }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
 }
